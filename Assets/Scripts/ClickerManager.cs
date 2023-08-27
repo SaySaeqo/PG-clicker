aa8b76b5164f92020dbf96d4604b9da632a07847
SzyRo151582 <szomyn3@gmail.com>
2023-08-27 20:30:42 +0200

    Lecturers Panel
    FIrst five lecturers added

--- Assets/Scripts/ClickerManager.cs
+++ Assets/Scripts/ClickerManager.cs
@@ -9,6 +9,8 @@ public class ClickerManager : MonoBehaviour
     public ClikcerUI clikcerUI;
     public Button clickerButton;
 
+    public Texture2D student;
+
     private int studentCounter = 0;
 
     private void Start()
@@ -21,5 +23,7 @@ public class ClickerManager : MonoBehaviour
     {
         studentCounter++;
         clikcerUI.UpdateUI(studentCounter);
+
+        Instantiate(student, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
     }
 }
